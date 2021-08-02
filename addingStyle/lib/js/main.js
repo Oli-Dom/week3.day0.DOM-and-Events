470be0d3d5239e5553a1bf4a67767cece66a9a95
Oli-Dom <87777222+Oli-Dom@users.noreply.github.com>
2021-08-02 16:39:38 -0400

    Update main.js

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,5 @@
+let rightSide = document.querySelector(".right_side");
+rightSide.style.backgroundColor =  "#d3c26f";
+
+let names= document.querySelector(".name");
+names.style.textShadow = "-6px 5px 2px #d3c26f "
